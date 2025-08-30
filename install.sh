37615ee98139b0d1c9df60a0ff646c7b9595578b
jimflower <68138221+jimflower@users.noreply.github.com>
2025-08-30 14:50:18 +1000

    Add installation script for chuck-menu

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+sudo apt-get update -y
+sudo apt-get install -y git bash coreutils findutils
+install -m0755 ./chuck-menu /usr/local/bin/chuck-menu
+echo "[OK] Installed. Run: chuck-menu"
 
